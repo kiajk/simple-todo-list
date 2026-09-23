ea5f1e7b2e9c4851dc6377133bb1d817d84726ff
kiarash <kiarashkermanian1382@gmail.com>
2026-09-24 01:09:10 +0330

    fix completed todo styling

--- script.js
+++ script.js
@@ -2,25 +2,71 @@ const todoInput = document.querySelector("#new-todo");
 const addButton = document.querySelector("#add-todo");
 const todoList = document.querySelector("#todo-list")
 
+const todos = [];
+let currentFilter = "all";
+const filterAll = document.querySelector("#filter-all")
+const filterActive = document.querySelector("#filter-active")
+const filterCompleted = document.querySelector("#filter-completed")
+function renderTodos(){
+     todoList.innerHTML = "";
+     const filteredTodos = todos.filter((todo) => {
+        if (currentFilter === "all") {
+            return true;
+        }
+        if (currentFilter === "active") {
+            return !todo.completed;
+        }
+        if (currentFilter === "completed") {
+            return todo.completed;
+        }
+     });
+     filteredTodos.forEach((todo) => {
+        const li = document.createElement("li");
+        li.textContent = todo.title;
+        if (todo.completed) {
+            li.classList.add("completed");
+        }
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "delete";
+        li.appendChild(deleteButton);
+        deleteButton.addEventListener("click" , () => {
+            event.stopPropagation();
+            todos.splice(todos.indexOf(todo),1)
+            renderTodos();
+        })
+        li.addEventListener("click" , () => {
+            todo.completed = !todo.completed;
+            renderTodos();
+        })
+        todoList.appendChild(li);
+     });
+}
+filterAll.addEventListener("click" , () => {
+    currentFilter = "all";
+    renderTodos();
+});
+filterActive.addEventListener("click" , () => {
+    currentFilter = "active";
+    renderTodos();
+});
+filterCompleted.addEventListener("click" , () => {
+    currentFilter = "completed"
+    renderTodos();
+})
 addButton.addEventListener("click" , () => {
     console.log("clicked");
     const todoText = todoInput.value;
     if (todoText.trim() === ""){
         return;
     }
-    const li = document.createElement("li")
-    li.textContent = todoText;
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "delete"
-    todoList.appendChild(li);
-    li.appendChild(deleteButton)
-    deleteButton.addEventListener("click", () => {
-        event.stopPropagation()
-        li.remove()
-    })
-    li.addEventListener("click", () => {
-        li.classList.toggle("completed")
-    });
+    const todo = {
+        id:Date.now(),
+        title: todoText,
+        completed:false
+    };
+    todos.push(todo);
+    renderTodos();
+    console.log(todos);
     console.log(todoText);
 });
 
